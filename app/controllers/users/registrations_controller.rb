17896f705e65ea50befb1a6fb22f8bd81ccb787a
Stephan De Grove <stephandegrove@gmail.com>
2017-11-14 18:35:11 +0100

    Sign up redicrect to profil form

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -3,7 +3,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
   protected
 
   def after_sign_up_path_for(resource)
-    '/profils/new' # Or :prefix_to_your_route
+   redirect_to '/profils/new' # Or :prefix_to_your_route
   end
 
   # before_action :configure_sign_up_params, only: [:create]
