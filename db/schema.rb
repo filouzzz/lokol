2dd8ec264de092fbdf827c8395631d4afa589aba
Jerome Carere <jer-car@hotmail.com>
2017-11-15 16:39:32 +0100

    Yo bitches, you can acces your profile through the navbar now

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,7 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 20171114161338) do
-<<<<<<< HEAD
 
-=======
-  
->>>>>>> 032e987ecbdb8dc86b599fd770640c9d2fa6143d
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
