1dc7a4faf8617240c5737e41d7f572ffabab18fa
hasna Oulad <ouladch@gmail.com>
2017-11-21 10:43:23 -0500

    resolve conflict

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -1,6 +1,8 @@
 class Campaign < ApplicationRecord
   has_attachments :photos, maximum: 5
   belongs_to :company
+  validates :campaign_objective, presence: true
+  validates :campaign_name, presence: true
   after_create :send_launch_campaign_email
 
   private
