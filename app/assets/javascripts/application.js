3852fffcf83bc192e94a246c12a3efa477ffcb2b
Stephan De Grove <stephandegrove@gmail.com>
2017-11-21 17:25:36 +0100

    just commit

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,7 +2,11 @@
 //= require jquery_ujs
 //= require bootstrap-sprockets
 //= require rails-ujs
-//= require jquery-fileupload/basic
+//= require jquery-fileupload
 //= require cloudinary/jquery.cloudinary
 //= require attachinary
+//= require jquery.ui.widget
+//= require jquery.iframe-transport
+//= require jquery.fileupload
+
 //= require_tree .
