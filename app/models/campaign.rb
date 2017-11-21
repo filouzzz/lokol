bffb05360e9495db082125db7ba92b97b933fa7e
hasna Oulad <ouladch@gmail.com>
2017-11-21 12:29:45 -0500

    adding update_campaign to mailers

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -4,11 +4,16 @@ class Campaign < ApplicationRecord
   validates :campaign_objective, presence: true
   validates :campaign_name, presence: true
   after_create :send_launch_campaign_email
+  after_update :send_update_email
 
   private
 
   def send_launch_campaign_email
     CampaignMailer.launch_campaign(self).deliver_now
+  end
+
+  def send_update_email
+    CampaignMailer.update_campaign(self).deliver_now
   end
 
 end
