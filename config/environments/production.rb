ebe8d108679df501b26bb552f2f3176b07376620
Philippe Ka <ruby@knafo.fr>
2017-11-22 12:33:02 +0100

    fixing a missing C in the word config in the prod env file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,6 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-onfig.action_mailer.delivery_method = :smtp
+config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options = { host: "lokol.herokuapp.com" }
   # Code is not reloaded between requests.
   config.cache_classes = true
